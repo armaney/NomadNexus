a415a62ae118f46cd0c1d7fcdaee138dfe971234
armaney <armax.ahmed@gmail.com>
2023-11-09 15:18:33 +0530

    changes

--- NomadNexus/src/App.jsx
+++ NomadNexus/src/App.jsx
@@ -1,3 +1,5 @@
+import React, {useState, useCallback} from 'react';
+
 
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 
@@ -6,32 +8,59 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
+
 
 import './App.css'
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(()=> {
+    setIsLoggedIn(true)
+  }, [])
+
+  const logout = useCallback(()=> {
+    setIsLoggedIn(false)
+  }, [])
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <>
+        <Route path="/" exact element={<Users />} />
+        <Route path="/:userId/places" exact element={<UserPlaces />} />
+        <Route path="/places/new" exact element={<NewPlace />} />
+        <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </>
+    );
+  } else {
+    routes = (
+      <>
+        <Route path="/" exact element={<Users />} />
+        <Route path="/:userId/places" exact element={<UserPlaces />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth" replace />} />
+      </>
+    );
+  }
+
+
   return (
-    <Router>
-      <MainNavigation />
-      
-      <main >
-      <Routes>
-      <Route path="/" exact element={<Users/>}></Route>
-      <Route path="/:userId/places" exact element={<UserPlaces />} />
-      <Route path="/places/new" exact element={<NewPlace />}></Route>
-      <Route path="/places/:placeId" element={<UpdatePlace />}></Route>
-      <Route
-        path="*"
-        element={<Navigate to="/" replace />}
-      />
-
-
-
-
-      </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}> 
+      <Router>
+        <MainNavigation />
+        <main >
+        <Routes>
+          {routes}
+        </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   )
 }
 
